ea1c4bdd7e01e991e97551ac5850163e9fa04999
yanzewei <shaunyan@sina.com>
2019-03-16 14:00:26 -0300

    different enviroment variables config

--- config.py
+++ config.py
@@ -3,7 +3,34 @@ import os
 # You need to replace the next values with the appropriate values for your configuration
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_ECHO = False
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:A00429842@localhost/shop"
-REDIS_URL = "redis://:www123456@localhost:6379/0"
+from dotenv import find_dotenv,load_dotenv
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+class Config:
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    #SQLALCHEMY_DATABASE_URI = "mysql://root:A00429842@localhost/shop"
+    #REDIS_URL = "redis://:www123456@localhost:6379/0"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_MYSQL_URL')
+    REDIS_URL = os.environ.get('DEV_REDIS_URL')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_MYSQL_URL')
+    REDIS_URL = os.environ.get('TEST_REDIS_URL')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQL_URL')
+    REDIS_URL = os.environ.get('TEST_REDIS_URL')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_cors import CORS
+from config import config
+import os
+from dotenv import find_dotenv,load_dotenv
 
 def create_app(config_filename):
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object(config_filename)
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    config_obj = config[os.getenv('FLASK_CONFIG') or 'default']
+    app.config.from_object(config_obj)
     
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
