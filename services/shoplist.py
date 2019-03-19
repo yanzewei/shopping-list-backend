4cf705e8838cf7f716d12645f7a88a7a3d35296f
yanzewei <shaunyan@sina.com>
2019-03-19 16:50:04 -0300

    handle shopping num

--- services/shoplist.py
+++ services/shoplist.py
@@ -9,36 +9,40 @@ shoplist_key_schema = ShoplistKey()
 class Shoplist(Resource):
     def get(self, uid):
         carts = ShopCart.get_by_uid(uid) 
-        phone_ids = []
-        game_machine_ids = []
-        for index in carts.keys():
-            temp = str(index).split('-')
+        phone_ids = {}
+        game_machine_ids = {}
+        for index,num in carts.items():
+            temp = index.split('-')
             if len(temp) == 2:
                 if int(temp[0]) == 1:
-                    phone_ids.append(temp[1])
+                    phone_ids[int(temp[1])] = int(num)
                 if int(temp[0]) == 2:
-                    game_machine_ids.append(temp[1])
+                    game_machine_ids[int(temp[1])] = int(num)
         result = []
         if len(phone_ids) > 0:
             phones = db.session.query(Brand, CategoryMobilephone).\
-                filter(db.and_(CategoryMobilephone.brand_id==Brand.id,CategoryMobilephone.id.in_(phone_ids))).all()
+                filter(db.and_(CategoryMobilephone.brand_id==Brand.id,CategoryMobilephone.id.in_(phone_ids.keys()))).all()
             phone_attributes = []
             for temp in phones:
-                temp[1].logo = temp[0].logo
-                temp[1].brand_name = temp[0].name
-                temp[1].category = temp[0].category
-                phone_attributes.append(temp[1])
+                if temp[1].id in phone_ids:
+                    temp[1].logo = temp[0].logo
+                    temp[1].brand_name = temp[0].name
+                    temp[1].category = temp[0].category
+                    temp[1].num = phone_ids[temp[1].id]
+                    phone_attributes.append(temp[1])
             result = product_schema.dump(phone_attributes).data
 
         if len(game_machine_ids) > 0:
             game_machine = db.session.query(Brand, CategoryGameMachine).\
-            filter(db.and_(CategoryGameMachine.brand_id==Brand.id,CategoryGameMachine.id.in_(game_machine_ids))).all()
+            filter(db.and_(CategoryGameMachine.brand_id==Brand.id,CategoryGameMachine.id.in_(game_machine_ids.keys()))).all()
             game_machine_attributes = [] 
             for temp in game_machine:
-                temp[1].logo = temp[0].logo
-                temp[1].brand_name = temp[0].name
-                temp[1].category = temp[0].category
-                game_machine_attributes.append(temp[1])
+                if temp[1].id in game_machine_ids:
+                    temp[1].logo = temp[0].logo
+                    temp[1].brand_name = temp[0].name
+                    temp[1].category = temp[0].category
+                    temp[1].num = game_machine_ids[temp[1].id]
+                    game_machine_attributes.append(temp[1])
             result = result + product_schema.dump(game_machine_attributes).data
         return {'status': 'success', 'data': result}, 200
 
@@ -51,9 +55,13 @@ class Shoplist(Resource):
         if errors:
             return errors, 422
         exsits_count = ShopCart.get_count(data['uid'], data['key'])
-        counts = exsits_count + data['nums']
-        if  counts > data['remain_count']:
-            return {'message': 'Cannot add number of the item over the current stock:'+str(data['remain_count'])}, 400
+        counts = int(exsits_count) +data['nums']
+        if  data['nums'] < 0:
+            return {'message': 'Cannot add number below zero'}, 400
+        elif data['nums'] > 99:
+            return {'message': 'The maximum limit is 99'}, 400
+        elif counts > data['remain_count']:
+            return {'message': 'Can not exceed the stock number'}, 400
         else:
             ShopCart.set_count(data['uid'], data['key'], counts)
         return { "status": 'success'}, 201
@@ -66,8 +74,12 @@ class Shoplist(Resource):
         data, errors = shoplist_count_schema.load(json_data)
         if errors:
             return errors, 422
-        if  data['nums'] > data['remain_count']:
-            return {'message': 'Cannot add number of the item over the current stock:'+str(data['remain_count'])}, 400
+        if  data['nums'] < 0:
+            return {'message': 'Cannot add number below zero'}, 400
+        elif data['nums'] > 99:
+            return {'message': 'The maximum limit is 99'}, 400
+        elif data['nums'] > data['remain_count']:
+            return {'message': 'Can not exceed the stock number'}, 400
         else:
             ShopCart.set_count(data['uid'], data['key'], data['nums'])
         return { "status": 'success'}, 204
