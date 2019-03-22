9ecd475f0b1c6786c440b44b9424eccb37b3b0ba
yanzewei <shaunyan@sina.com>
2019-03-22 03:25:41 -0300

    boot.sh

--- boot.sh
+++ boot.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
+python -m venv venv
+
 source venv/bin/activate
+
+pip install -r requirements/requirements.txt
 
 python migrate.py db upgrade
 
-exec gunicorn -b :5000 --access-logfile - --error-logfile - run:app
+gunicorn -b :5000 --access-logfile - --error-logfile - run:app --daemon
+# can also be:
+# gunicorn -b :5000 --access-logfile - --error-logfile - run:app
+# or
+# python run.py
